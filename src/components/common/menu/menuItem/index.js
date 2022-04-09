9a1d2acea53aca17c405ea228fa2fd462f01cde8
betteriq <betteriq@yahoo.com>
2022-04-09 13:53:09 +0430

    third

---
+++ src/components/common/menu/menuItem/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const MenuItem = ({ item }) => {
+  const { id, name, info, price, img } = item;
+  return (
+    <div className="item">
+      <img src={img} alt="img" />
+      <div className="item_head_desk">
+        <p className="head_desk_name">{name}</p>
+        <p className="head_desk_info">
+          <small>{info}</small>
+        </p>
+      </div>
+      <div className="item_foot_desk">
+        <span className="foot_desk_price">${price}</span>
+        <button>Add +</button>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
